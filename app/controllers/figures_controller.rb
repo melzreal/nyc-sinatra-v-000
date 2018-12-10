8b7ad844b6f8119843ea7827616e8db979bb5e60
Melissa Real <melissa.real@hotmail.com>
2018-12-10 12:58:57 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,31 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/show_figures'
+  end
+
+   get '/figures/new' do
+     erb :'figures/new'
+   end
+
+   post '/figures' do
+
+     @figure = Figure.create(:name => params[:figure][:name])
+     @figure.titles << Title.find_or_create_by(params[:figure][:title_ids]) << Title.find_or_create_by(params[:title])
+     @figure.landmarks << Landmark.find_or_create_by(params[:figure][:landmark_ids])
+     @figure.landmarks.each do |land|
+      land.name << params[:landmark][:name]
+      land.year_completed << params[:landmark][:year_completed].to_i
+    end
+
+     @figure.save
+
+     redirect '/figures'
+   end
+
+
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,15 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/show'
+  end
+
+   get '/landmarks/new' do
+     erb :'landmarks/new'
+   end
+
+
 end
